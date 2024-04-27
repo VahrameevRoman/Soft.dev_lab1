7656273df650164c47ed8edfdb5d3523e5396a11
Roman Vahrameev <romance.vah@mail.ru>
2024-04-27 17:09:44 +0300

    Добавьте файлы проекта.

---
+++ Soft.dev_lab1/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Soft.dev_lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxAlpha = new System.Windows.Forms.TextBox();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.textBoxS = new System.Windows.Forms.TextBox();
+            this.labelS = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Soft.dev_lab1.Properties.Resources.picture;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(363, 115);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(623, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Вычисление прлощади парраллелограмма по формуле:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(18, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(245, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите переменные";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "a";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "b";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 317);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "α";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxA.Location = new System.Drawing.Point(43, 217);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 31);
+            this.textBoxA.TabIndex = 6;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxB.Location = new System.Drawing.Point(43, 266);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 31);
+            this.textBoxB.TabIndex = 7;
+            // 
+            // textBoxAlpha
+            // 
+            this.textBoxAlpha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxAlpha.Location = new System.Drawing.Point(43, 317);
+            this.textBoxAlpha.Name = "textBoxAlpha";
+            this.textBoxAlpha.Size = new System.Drawing.Size(100, 31);
+            this.textBoxAlpha.TabIndex = 8;
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ButtonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonStart.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ButtonStart.Location = new System.Drawing.Point(23, 379);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(149, 42);
+            this.ButtonStart.TabIndex = 9;
+            this.ButtonStart.Text = "Start";
+            this.ButtonStart.UseVisualStyleBackColor = false;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // textBoxS
+            // 
+            this.textBoxS.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxS.Location = new System.Drawing.Point(252, 385);
+            this.textBoxS.Name = "textBoxS";
+            this.textBoxS.ReadOnly = true;
+            this.textBoxS.Size = new System.Drawing.Size(100, 31);
+            this.textBoxS.TabIndex = 11;
+            this.textBoxS.Visible = false;
+            // 
+            // labelS
+            // 
+            this.labelS.AutoSize = true;
+            this.labelS.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelS.Location = new System.Drawing.Point(206, 388);
+            this.labelS.Name = "labelS";
+            this.labelS.Size = new System.Drawing.Size(40, 25);
+            this.labelS.TabIndex = 10;
+            this.labelS.Text = "S=";
+            this.labelS.UseMnemonic = false;
+            this.labelS.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxS);
+            this.Controls.Add(this.labelS);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.textBoxAlpha);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Вычисление площади парралелограмма";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxAlpha;
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.TextBox textBoxS;
+        private System.Windows.Forms.Label labelS;
+    }
+}
+
---
+++ Soft.dev_lab1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soft.dev_lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            bool check = true;
+            if (CheckForPositiveNumber(textBoxA.Text) == false)
+            {
+                check = false;
+            }
+            else if (CheckForPositiveNumber(textBoxB.Text) == false)
+            {
+                check = false;
+            }
+            else if (CheckForPositiveNumber(textBoxAlpha.Text) == false)
+            {
+                check = false;
+            }
+            else if (double.Parse(textBoxAlpha.Text) >= 180)
+            {
+                check = false;
+            }
+            if (!check)
+            {
+                labelS.Visible = false;
+                textBoxS.Visible = false;
+                MessageBox.Show(
+                "Введите корректные значения переменных.",
+                "Ошибка",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else Calculation();
+        }
+
+        private bool CheckForPositiveNumber(string str)
+        {
+            double number;
+            if (double.TryParse(str, out number) && (number > 0)) return true;
+            return false;
+        }
+
+        private void Calculation()
+        {
+            double a = double.Parse(textBoxA.Text);
+            double b = double.Parse(textBoxB.Text);
+            double alpha = double.Parse(textBoxAlpha.Text);
+            double result = Math.Round(a * b * Math.Sin(alpha/57.2958), 5);
+            if (result > 0)
+            {
+                labelS.Visible = true;
+                textBoxS.Visible = true;
+                textBoxS.Text = Convert.ToString(result);
+            }
+            else
+            {
+                labelS.Visible = false;
+                textBoxS.Visible = false;
+                MessageBox.Show(
+                "Параллелограмма с введёнными переменными не может существовать.",
+                "Ошибка",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+            "Выйти из программы?",
+            "Внимание",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Error,
+            MessageBoxDefaultButton.Button1,
+            MessageBoxOptions.DefaultDesktopOnly);
+            if (result == System.Windows.Forms.DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
